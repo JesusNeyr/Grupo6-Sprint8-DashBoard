058c610f2ce7f7053e27aef59f2782b462a356af
sosajuani <sosajuanignacio17@gmail.com>
2022-03-30 14:03:55 -0300

    user page

--- src/components/pages/Users.js
+++ src/components/pages/Users.js
@@ -1,21 +1,18 @@
 import React, { useEffect, useState } from "react"; 
 const Users = () => {
-    const [cardUsersCount,setCardUsersCount] = useState([]);
+    const [Users,setUsers] = useState([]);
     
     useEffect(()=>{
-
         fetch('/api/users')
         .then(response => {
             return response.json()
         })
         .then(data => {
-           
-            console.log(data);
-            setCardUsersCount(data.total)
+            setUsers(data.users.data)
         })
+        
+    },[Users]);
 
-    },[cardUsersCount]);
-    
 
     return ( 
         <div className="gridUserContPage">
@@ -28,24 +25,18 @@ const Users = () => {
                 <div>Apellido</div>
                 <div>Email</div>
             </div>
-            <div className="gridUserItemPage">
-                <div>1</div>
-                <div>Usuario 1</div>
-                <div>Apellido 1</div>
-                <div>email@pruebas.com</div>
-            </div>
-            <div className="gridUserItemPage">
-                <div>2</div>
-                <div>Usuario 2</div>
-                <div>Apellido 2</div>
-                <div>email@pruebas.com</div>
-            </div>
-            <div className="gridUserItemPage">
-                <div>3</div>
-                <div>Usuario 3</div>
-                <div>Apellido 3</div>
-                <div>email@pruebas.com</div>
-            </div>
+            {Users.length === 0 ? "cargando..." : null}
+            { Users.map((user,i) =>{
+                return (
+                    <div key={user.first_name+i} className="gridUserItemPage">
+                        <div>{user.id}</div>
+                        <div>{user.first_name}</div>
+                        <div>{user.last_name}</div>
+                        <div>{user.email}</div>
+                    </div>
+                )
+                })
+            }
         </div>
     );
 }
