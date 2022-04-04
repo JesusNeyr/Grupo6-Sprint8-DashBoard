8514d552f5a08f954f3926dbb3cbc6c14a9d1bd2
sosajuani <sosajuanignacio17@gmail.com>
2022-04-04 13:18:31 -0300

    solucion de bugs

--- src/components/CategorysHome.js
+++ src/components/CategorysHome.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const categoryHome = () => {
-    let category = ['categoría1','categoría2','categoría3','categoría4','categoría5']
+const CategoryHome = () => {
+    
+    const [estado, setEstado] = useState([])
+    useEffect(()=>{
+        fetch('/api/categories/products')
+        .then(response => response.json())
+        .then(data=> setEstado(data.productsCat.data))
+    }, [])
     return ( 
         <div>
             <ul className="CardMorePrinCatUl">
-                {/* <li className="CardMorePrinCatLi">Categoria 1</li>
-                <li className="CardMorePrinCatLi">Categoria 2</li>
-                <li className="CardMorePrinCatLi">Categoria 3</li> */}
-                {category.map((category,i) => <li key={category+i} className="CardMorePrinCatLi">{category}</li>)}
+                {/* {category.map((categoryItem,i) => <li key={i} className="CardMorePrinCatLi">{categoryItem.category}</li>)} */}
+                {estado.map((item,i)=> <li key={i} className="CardMorePrinCatLi"><p>{item.category}</p><p>Productos: {item.productsCount}</p></li>)}
             </ul>
         </div>
      );
 }
  
-export default categoryHome;
+export default CategoryHome;
--- src/components/LastProductHome.js
+++ src/components/LastProductHome.js
@@ -1,14 +1,12 @@
 import React,{useEffect, useState} from "react";
-import ImageProduct from '../asset/images/default.jpg';
-
 
 const LastProductHome = () => {
 
     const [product1,setVariableActualizaProduct1] = useState([])
     useEffect(()=>{
              fetch('/api/products/lastProduct')
-             .then(response=>response.json)
-             .then(data=> setVariableActualizaProduct1(data))
+             .then(response=>response.json())
+             .then(data=> setVariableActualizaProduct1(data.data))
     }, [])
     console.log(product1)
 
@@ -18,7 +16,7 @@ const LastProductHome = () => {
                 <img src={product1.imagen} alt="imagen de product" />
             </div>
             <div className="cardMorePrinTitleProduct">{product1.name}</div>
-            <div className="btnDashboard">Mas info</div>
+            <a href={product1.url} rel="noreferrer" target="_blank"><div className="btnDashboard">Mas info</div></a>
         </React.Fragment>
      );
 }
